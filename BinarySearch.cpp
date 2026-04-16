3c842acb4ba59bb862157ded50927b9690619556
khansa-qonita <khansaqonita420@gmail.com>
2026-04-16 11:13:45 +0700

    menambahkan perulangan pencarian dan kondisi elemen ditemukan atau tidak ditemukan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -80,7 +80,38 @@ void binarySearch()
         cout << "masukkan elemen yang ingin dicari : ";
         cin >> x;
 
-        int low
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high ) / 2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] elemen" << x << "ditemukan pada indeks" << mid << "\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while ( low <= high );
+
+        if (low > high)
+        {
+            cout << "\n[x] elemen" << x << "tidak ditemukan dalam array. \n";
+        }
+
+
+        cout << "\nIngin mencari lagi? (y/n) :";
+        cin >> ulang;
+
+    
     }
 }
 
