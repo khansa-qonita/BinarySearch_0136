ca09d40a55ac814f2b9de601cea31e90f6ac26fb
khansa-qonita <khansaqonita420@gmail.com>
2026-04-16 10:42:40 +0700

    mengimplementasikan algoritma Bubble Sort untuk mengurutkan array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -33,7 +33,22 @@ void input()
     }
 }
 
-void bubble
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+    
+}
 
 
 
